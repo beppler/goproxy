b8407caa841e6c078879f192e475ad59293a41b7
Carlos Alberto Costa Beppler <carlos.beppler@mps.com.br>
2017-09-13 10:45:10 -0300

    Incluir a opção de fazer um MITM para poder saber o que está passando em conexões HTTPS.

--- goproxy.go
+++ goproxy.go
@@ -9,10 +9,14 @@ import (
 )
 
 func main() {
-	verbose := flag.Bool("v", true, "should every proxy request be logged to stdout")
 	addr := flag.String("addr", ":8080", "proxy listen address")
+	mitm := flag.Bool("m", false, "Enable MITM for HTTPS connections")
+	verbose := flag.Bool("v", true, "should every proxy request be logged to stdout")
 	flag.Parse()
 	proxy := goproxy.NewProxyHttpServer()
+	if *mitm {
+		proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
+	}
 	proxy.Verbose = *verbose
 	log.Fatal(http.ListenAndServe(*addr, proxy))
 }
